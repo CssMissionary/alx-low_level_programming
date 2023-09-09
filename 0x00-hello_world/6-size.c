cb64fd5c61da79572de59e5d16fa524f295c91c6
Nwogu Ifeanyi <mrlavezzi3@gmail.com>
2023-09-08 19:55:09 -0700

    100-intel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,12 +6,22 @@
  * Return: 0 on success
  */
 
-int main(void)
-{
-	printf("Size of a char: %lu byte(s)", sizeof(char));
-	printf("Size of an int: %lu byte(s)", sizeof(int));
-	printf("Size of a long int: %lu byte(s)", sizeof(long int));
-	printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
-	printf("Size of a float: %lu byte(s)", sizeof(float));
-	return (0);
+int main(void) 
+{ 
+  #ifdef __x86_64__
+  printf("This is a 64-bit machine.\n");
+  #else
+  printf("This is a 32-bit machine.\n");
+  #endif
+
+  printf("Size of char: %lu bytes\n", sizeof(char));
+  printf("Size of int: %lu bytes\n", sizeof(int));
+  printf("Size of long: %lu bytes\n", sizeof(long));
+  printf("Size of long long: %lu bytes\n", sizeof(long long));
+  printf("Size of float: %lu bytes\n", sizeof(float));
+  printf("Size of double: %lu bytes\n", sizeof(double));
+  printf("Size of long double: %lu bytes\n", sizeof(long double));
+    
+  return (0);
 }
+
