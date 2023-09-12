40fd5eb71e12c9d67182056783acaea3f8efcf10
Nwogu Ifeanyi <mrlavezzi3@gmail.com>
2023-09-12 08:06:49 -0700

    done

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -11,18 +12,19 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		putchar('+');
 		return (1);
-	{
+	}
 	else if (n == 0)
 	{
-		_putchar(48);
+		putchar('0');
 		return (0);
 	}
-	else (n < 0)
+	else
 	{
-		_putchar('-');
-	}
+		putchar('-');
 		return (-1);
+	}
 }
+
 
